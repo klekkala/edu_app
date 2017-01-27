5411f1a6af800c02cd4c673ef12229146d9f24dd
Sandeep Adireddy <Sandeep Adireddy>
2017-01-27 08:02:07 +0530

    added links in urls.py for nav bar items

--- app/models.py
+++ app/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Test_Data(models.Model):
+   id = models.primary_key=True
+   name = models.CharField(max_length = 50)
+   age = models.IntegerField()
+   gender = models.CharField(max_length = 50)
+   score = models.IntegerField()
+   subject = models.CharField(max_length = 150)
+   region = models.CharField(max_length = 50)
