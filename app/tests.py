46f022f1e48e5c3e31986c95522a621fdd5fefdb
kiran4399 <kiran4399@gmail.com>
2017-02-10 15:58:21 +0530

    test.py added

--- app/tests.py
+++ app/tests.py
@@ -1,44 +1,39 @@
 from django.test import TestCase
-
-# Create your tests here.
-
-from pyvows import Vows, expect
-from django_pyvows.context import DjangoHTTPContext
-
-DjangoHTTPContext.start_environment("tddapp.settings")
-
-from accounts.views import login
-from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
+class LoginTest(TestCase):
 
-class TestLoginForm(TestCase):
+    def test_register(self):
+        user = User.objects.create_user('new', 'new@gmail.com', 'new')
+        self.assertEqual(1+1, 2)
 
-    def topic(self):
-        self.start_server()
-
-
-class TestAddAuth(TestCase):
-
-    def topic(self):
-        self.start_server()
-
-
-class TestUpdateData(TestCase):
-
-    def topic(self):
-        self.start_server()
+    def test_login(self):
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+        response = self.client.login(username= 'temporary', password= 'temporary')
+        self.assertEqual(response, True)
 
 
+    def test_hacker(self):
+        response = self.client.login(username= 'hacker', password= 'hacking')
+        self.assertEqual(response, False)
 
-class TestNotifications(TestCase):
 
-    def topic(self):
-        self.start_server()
+    def test_emptypass(self):
+        response = self.client.login(username= 'temporary', password= '')
+        self.assertEqual(response, False)
+
+
+    def test_emptyuser(self):
+        response = self.client.login(username= '', password= 'temporary')
+        self.assertEqual(response, False)
 
 
 
-class TestRemoveUsers(TestCase):
+class RemoveTest(TestCase):
 
-    def topic(self):
-        self.start_server()
+    def test_removeuser(self):
+        user = User.objects.delete_user('temporary', 'temporary@gmail.com', 'temporary')
+        response = self.client.login(username= 'temporary', password= 'temporary')
+        self.assertEqual(response, True)
