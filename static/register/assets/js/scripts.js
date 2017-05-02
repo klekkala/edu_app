a2986bf308906b51e2620d897619160183474749
kiran4399 <kiran4399@gmail.com>
2017-05-02 17:55:00 +0530

    tables display from firebase

--- static/register/assets/js/scripts.js
+++ static/register/assets/js/scripts.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
         Fullscreen background
     */
     $.backstretch("assets/img/backgrounds/1.jpg");
-    /*
+    
     /*
         Login form validation
     */
